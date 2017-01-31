9cdaba4b1a2c175bedd7a087f8f56026aef7e8f7
h0r0man <roman.horokhovatskyy@gmail.com>
2017-01-31 17:33:14 +0100

    Added query to target every fourth post differently

--- index.php
+++ index.php
@@ -1,6 +1,14 @@
 <?php get_header(); ?>
 
-<?php if (have_posts()): ?>
+<?php
+  $args = array(
+    'post_type' => 'post',
+    'nopaging' => true
+  );
+  $the_query = new WP_Query($args);
+  if ($the_query->have_posts()):
+  $count = 1;
+?>
 
   <section class="index">
 
@@ -14,9 +22,21 @@
       <h2><?php esc_html_e('News', 'landhocoworking'); ?></h2>
     <?php endif; ?>
           
-    <?php while (have_posts()): the_post(); ?>
-      <?php get_template_part('template-parts/content', 'preview'); ?>
-    <?php endwhile; ?>
+    <?php
+      while ($the_query->have_posts()): $the_query->the_post();
+    ?>
+
+      <?php if (($count % 4) == 0): ?>
+        <?php get_template_part('template-parts/content', 'preview-large'); ?>
+      <?php else: ?>
+        <?php get_template_part('template-parts/content', 'preview'); ?>
+      <?php endif; ?>
+      
+    <?php
+      $count++;
+      endwhile;
+      wp_reset_postdata();
+    ?>
 
   </section>
         
---
+++ template-parts/content-preview-large.php
@@ -0,0 +1,7 @@
+<article class="preview preview--large">
+  <a rel="bookmark" href="<?php the_permalink(); ?>">
+    <?php the_post_thumbnail(); ?>
+    <h3><?php the_title(); ?></h3>
+    <?php the_excerpt(); ?>
+  </a>
+</article>
