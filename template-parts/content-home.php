bd1e82adc821be15c2c665a76d578b9943ce2c29
h0r0man <roman.horokhovatskyy@gmail.com>
2017-02-10 17:32:50 +0100

    Improved board part
    Separated columns from card for better browser compatibility

--- template-parts/content-home.php
+++ template-parts/content-home.php
@@ -25,97 +25,105 @@
   ?>
     <div class="board<?php if (get_sub_field('board__layout') == 'one'): ?> board--one<?php elseif (get_sub_field('board__layout') == 'two'): ?> board--two<?php elseif (get_sub_field('board__layout') == 'three'): ?> board--three<?php elseif (get_sub_field('board__layout') == 'four'): ?> board--four<?php endif; ?>">
       <div class="container">
-        <div class="layout__row">
+        <div class="layout__row layout__row--box">
 
-          <article class="card card__1 card--large layout__xs__12 layout__sm__6 layout__md__6 layout__lg__6">
-            <a href="<?php the_sub_field('board__large__link'); ?>" rel="bookmark" class="card__link">
-              <div class="card__container">
-                <div class="card__image">
-                  <?php echo wp_get_attachment_image(get_sub_field('board__large__image'), 'thumbnail-large', false, array()); ?>
+          <div class="layout__xs__12 layout__sm__6 layout__md__6 layout__lg__6">
+            <article class="card card__1 card--large">
+              <a href="<?php the_sub_field('board__large__link'); ?>" rel="bookmark" class="card__link">
+                <div class="card__container">
+                  <div class="card__image">
+                    <?php echo wp_get_attachment_image(get_sub_field('board__large__image'), 'thumbnail-large', false, array()); ?>
+                  </div>
+                  <div class="card__content">
+                    <h3><?php the_sub_field('board__large__title'); ?></h3>
+                    <p><?php the_sub_field('board__large__text'); ?></p>
+                  </div>
                 </div>
-                <div class="card__content">
-                  <h3><?php the_sub_field('board__large__title'); ?></h3>
-                  <p><?php the_sub_field('board__large__text'); ?></p>
-                </div>
+              </a>
+            </article>
+          </div>
+
+          <div class="layout__xs__12 layout__sm__6 layout__md__6 layout__lg__6">
+            <div class="layout__row layout__row--box">
+
+              <div class="layout__xs__12 layout__sm__12 layout__md__12 layout__lg__12">
+                <article class="card card__2 card--medium">
+                  <a href="<?php the_sub_field('board__medium__link'); ?>" rel="bookmark" class="card__link">
+                    <div class="card__container">
+                      <div class="card__image">
+                        <?php echo wp_get_attachment_image(get_sub_field('board__medium__image'), 'medium', false, array()); ?>
+                      </div>
+                      <div class="card__content">
+                        <h3 class="normalized bold"><?php the_sub_field('board__medium__title'); ?></h3>
+                      </div>
+                    </div>
+                  </a>
+                </article>
               </div>
-            </a>
-          </article>
 
-          <div class="card__group layout__xs__12 layout__sm__6 layout__md__6 layout__lg__6">
-            <div class="layout__row">
-
-              <article class="card card__2 card--medium layout__xs__12 layout__sm__12 layout__md__12 layout__lg__12">
-                <a href="<?php the_sub_field('board__medium__link'); ?>" rel="bookmark" class="card__link">
-                  <div class="card__container">
-                    <div class="card__image">
-                      <?php echo wp_get_attachment_image(get_sub_field('board__medium__image'), 'medium', false, array()); ?>
-                    </div>
-                    <div class="card__content">
-                      <h3 class="normalized bold"><?php the_sub_field('board__medium__title'); ?></h3>
-                    </div>
-                  </div>
-                </a>
-              </article>
-
-              <article class="card card__3 card--small<?php if (get_sub_field('board__small__1__type') == 'image'): ?> card--image<?php elseif (get_sub_field('board__small__1__type') == 'video'): ?> card--video<?php elseif (get_sub_field('board__small__1__type') == 'text'): ?> card--text<?php endif; ?> layout__xs__6 layout__sm__6 layout__md__6 layout__lg__6">
-                <a href="<?php the_sub_field('board__small__1__link'); ?>" rel="bookmark" class="card__link">
-                  <div class="card__container">
-                    <?php if (get_sub_field('board__small__1__type') == 'video'): ?>
-                      <div class="card__play">
-                        <!-- TODO: place play icon here -->
-                      </div>
-                    <?php endif; ?>
-
-                    <?php if (get_sub_field('board__small__1__type') == 'image'): ?>
-                      <div class="card__image">
-                        <?php echo wp_get_attachment_image(get_sub_field('board__small__1__image'), 'thumbnail', false, array()); ?>
-                      </div>
-                    <?php endif; ?>
-
-                    <div class="card__content<?php if (get_sub_field('board__small__1__type') == 'text'): ?> card__content--truncate<?php endif; ?>">
-                      <?php if (get_sub_field('board__small__1__type') == 'image' || get_sub_field('board__small__1__type') == 'video'): ?>
-                        <h3 class="normalized bold"><?php the_sub_field('board__small__1__title'); ?></h3>
+              <div class="layout__xs__6 layout__sm__6 layout__md__6 layout__lg__6">
+                <article class="card card__3 card--small<?php if (get_sub_field('board__small__1__type') == 'image'): ?> card--image<?php elseif (get_sub_field('board__small__1__type') == 'video'): ?> card--video<?php elseif (get_sub_field('board__small__1__type') == 'text'): ?> card--text<?php endif; ?>">
+                  <a href="<?php the_sub_field('board__small__1__link'); ?>" rel="bookmark" class="card__link">
+                    <div class="card__container">
+                      <?php if (get_sub_field('board__small__1__type') == 'video'): ?>
+                        <div class="card__play">
+                          <!-- TODO: place play icon here -->
+                        </div>
                       <?php endif; ?>
 
-                      <?php if (get_sub_field('board__small__1__type') == 'text'): ?>
-                        <h3><?php the_sub_field('board__small__1__title'); ?></h3>
-                        <p><?php the_sub_field('board__small__1__text'); ?></p>
+                      <?php if (get_sub_field('board__small__1__type') == 'image'): ?>
+                        <div class="card__image">
+                          <?php echo wp_get_attachment_image(get_sub_field('board__small__1__image'), 'thumbnail', false, array()); ?>
+                        </div>
                       <?php endif; ?>
+
+                      <div class="card__content<?php if (get_sub_field('board__small__1__type') == 'text'): ?> card__content--truncate<?php endif; ?>">
+                        <?php if (get_sub_field('board__small__1__type') == 'image' || get_sub_field('board__small__1__type') == 'video'): ?>
+                          <h3 class="normalized bold"><?php the_sub_field('board__small__1__title'); ?></h3>
+                        <?php endif; ?>
+
+                        <?php if (get_sub_field('board__small__1__type') == 'text'): ?>
+                          <h3><?php the_sub_field('board__small__1__title'); ?></h3>
+                          <p><?php the_sub_field('board__small__1__text'); ?></p>
+                        <?php endif; ?>
+                      </div>
                     </div>
-                  </div>
-                </a>
-              </article>
+                  </a>
+                </article>
+              </div>
 
-              <article class="card card__4 card--small<?php if (get_sub_field('board__small__2__type') == 'image'): ?> card--image<?php elseif (get_sub_field('board__small__2__type') == 'video'): ?> card--video<?php elseif (get_sub_field('board__small__2__type') == 'text'): ?> card--text<?php endif; ?> layout__xs__6 layout__sm__6 layout__md__6 layout__lg__6">
-                <a href="<?php the_sub_field('board__small__2__link'); ?>" rel="bookmark" class="card__link">
-                  <div class="card__container">
-                    <?php if (get_sub_field('board__small__2__type') == 'video'): ?>
-                      <div class="card__play">
-                        <svg>
-                          <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#play"></use>
-                        </svg>
-                      </div>
-                    <?php endif; ?>
+              <div class="layout__xs__6 layout__sm__6 layout__md__6 layout__lg__6">
+                <article class="card card__4 card--small<?php if (get_sub_field('board__small__2__type') == 'image'): ?> card--image<?php elseif (get_sub_field('board__small__2__type') == 'video'): ?> card--video<?php elseif (get_sub_field('board__small__2__type') == 'text'): ?> card--text<?php endif; ?>">
+                  <a href="<?php the_sub_field('board__small__2__link'); ?>" rel="bookmark" class="card__link">
+                    <div class="card__container">
+                      <?php if (get_sub_field('board__small__2__type') == 'video'): ?>
+                        <div class="card__play">
+                          <svg>
+                            <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#play"></use>
+                          </svg>
+                        </div>
+                      <?php endif; ?>
 
-                    <?php if (get_sub_field('board__small__2__type') == 'image' || get_sub_field('board__small__2__type') == 'video'): ?>
-                      <div class="card__image">
-                        <?php echo wp_get_attachment_image(get_sub_field('board__small__2__image'), 'thumbnail', false, array()); ?>
-                      </div>
-                    <?php endif; ?>
-
-                    <div class="card__content<?php if (get_sub_field('board__small__2__type') == 'text'): ?> card__content--truncate<?php endif; ?>">
                       <?php if (get_sub_field('board__small__2__type') == 'image' || get_sub_field('board__small__2__type') == 'video'): ?>
-                        <h3 class="normalized bold"><?php the_sub_field('board__small__2__title'); ?></h3>
+                        <div class="card__image">
+                          <?php echo wp_get_attachment_image(get_sub_field('board__small__2__image'), 'thumbnail', false, array()); ?>
+                        </div>
                       <?php endif; ?>
 
-                      <?php if (get_sub_field('board__small__2__type') == 'text'): ?>
-                        <h3><?php the_sub_field('board__small__2__title'); ?></h3>
-                        <p><?php the_sub_field('board__small__2__text'); ?></p>
-                      <?php endif; ?>
+                      <div class="card__content<?php if (get_sub_field('board__small__2__type') == 'text'): ?> card__content--truncate<?php endif; ?>">
+                        <?php if (get_sub_field('board__small__2__type') == 'image' || get_sub_field('board__small__2__type') == 'video'): ?>
+                          <h3 class="normalized bold"><?php the_sub_field('board__small__2__title'); ?></h3>
+                        <?php endif; ?>
+
+                        <?php if (get_sub_field('board__small__2__type') == 'text'): ?>
+                          <h3><?php the_sub_field('board__small__2__title'); ?></h3>
+                          <p><?php the_sub_field('board__small__2__text'); ?></p>
+                        <?php endif; ?>
+                      </div>
                     </div>
-                  </div>
-                </a>
-              </article>
+                  </a>
+                </article>
+              </div>
 
             </div>
           </div>
