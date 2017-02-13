d0b094c7585cef49749b896cd345fa9f58a19bbc
h0r0man <roman.horokhovatskyy@gmail.com>
2017-02-13 16:22:02 +0100

    Added global headings part to index

--- index.php
+++ index.php
@@ -12,28 +12,7 @@
 
   <section class="index">
 
-    <?php if (is_category()): ?>
-
-      <h2 class="h1"><?php single_cat_title(); ?></h2>
-
-    <?php elseif (is_tag()): ?>
-
-      <h2 class="h1"><?php single_tag_title(); ?></h2>
-
-    <?php elseif (is_search()): ?>
-
-      <h2 class="h1"><?php esc_html_e('Cerca', 'landhocoworking'); ?></h2>
-      <p class="lead"><?php printf(__('Risultati per: %s', 'landhocoworking'), get_search_query()); ?></p>
-
-    <?php else: ?>
-
-      <h2 class="h1"><?php esc_html_e('News', 'landhocoworking'); ?></h2>
-
-      <?php if (get_field('page_lead', get_option('page_for_posts'))): ?>
-        <p class="lead"><?php the_field('page_lead', get_option('page_for_posts')); ?></p>
-      <?php endif; ?>
-
-    <?php endif; ?>
+    <?php get_template_part('template-parts/content', 'heading'); ?>
 
     <?php while ($the_query->have_posts()): $the_query->the_post(); ?>
 
