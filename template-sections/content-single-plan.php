87502725285028cacb95872fbdd61e5d3696c297
h0r0man <roman.horokhovatskyy@gmail.com>
2017-02-17 11:43:27 +0100

    Reordered html structure of single plan

--- template-sections/content-single-plan.php
+++ template-sections/content-single-plan.php
@@ -21,38 +21,38 @@
         <?php endif; ?>
 
         <div class="layout__xs__12 layout__sm__8 layout__md__8 layout__lg__8">
+
+          <?php if (has_post_thumbnail()): ?>
+            <div class="hero">
+              <div class="hero__image">
+                <?php the_post_thumbnail('medium'); ?>
+              </div>
+            </div>
+          <?php endif; ?>
+          
           <div class="plan__body">
-
-            <?php if (has_post_thumbnail()): ?>
-              <div class="hero">
-                <div class="hero__image">
-                  <?php the_post_thumbnail('medium'); ?>
-                </div>
-              </div>
-            <?php endif; ?>
-
             <?php the_field('plan_text'); ?>
-            
-            <?php if (have_rows('plan_features')): ?>
-
-              <hr>
-
-              <div class="plan__features">
-
-                <h3 class="hidden"><?php esc_html_e('Caratteristiche', 'landhocoworking'); ?></h3>
-                <ul class="plan__features__items layout__row">
-                  <?php while (have_rows('plan_features')) : the_row(); ?>
-                    <li class="plan__features__item layout__xs__12 layout__sm__6 layout__md__6 layout__lg__6">
-                      <h4 class="h3"><?php the_sub_field('plan_features_title'); ?></h4>
-                      <p class="small"><?php the_sub_field('plan_features_text'); ?></p>
-                    </li>
-                  <?php endwhile; ?>
-                </ul>
-
-              </div>
-            <?php endif; ?>
-            
           </div>
+          
+          <?php if (have_rows('plan_features')): ?>
+
+            <hr>
+
+            <div class="plan__features">
+
+              <h3 class="hidden"><?php esc_html_e('Caratteristiche', 'landhocoworking'); ?></h3>
+              <ul class="plan__features__items layout__row">
+                <?php while (have_rows('plan_features')) : the_row(); ?>
+                  <li class="plan__features__item layout__xs__12 layout__sm__6 layout__md__6 layout__lg__6">
+                    <h4 class="h3"><?php the_sub_field('plan_features_title'); ?></h4>
+                    <p class="small"><?php the_sub_field('plan_features_text'); ?></p>
+                  </li>
+                <?php endwhile; ?>
+              </ul>
+
+            </div>
+          <?php endif; ?>
+          
         </div>
       </div>
     </div>
