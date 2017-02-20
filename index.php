5ec46ea3b89ce11b437af142211111adec87ffe3
h0r0man <roman.horokhovatskyy@gmail.com>
2017-02-20 14:19:56 +0100

    Added index page

--- index.php
+++ index.php
@@ -14,25 +14,47 @@
 
     <?php get_template_part('template-parts/content', 'heading'); ?>
 
-    <?php while ($the_query->have_posts()): $the_query->the_post(); ?>
+    <div class="container">
+      <div class="layout__row layout__row--box">
+        <?php while ($the_query->have_posts()): $the_query->the_post(); ?>
+          
+          <div class="layout__xs__12<?php if (($count % 3) == 0): ?> layout__sm__12 layout__md__4 layout__lg__4<?php elseif (($count % 4) == 0): ?> layout__sm__6 layout__md__12 layout__lg__12<?php else: ?> layout__sm__6 layout__md__4 layout__lg__4<?php endif; ?>">
 
-      <?php if (($count % 4) == 0): ?>
-        <?php get_template_part('template-sections/content', 'preview-large'); ?>
-      <?php else: ?>
-        <?php get_template_part('template-sections/content', 'preview'); ?>
-      <?php endif; ?>
-      
-    <?php
-      $count++;
-      endwhile;
-      wp_reset_postdata();
-    ?>
+            <article class="card card--large card--preview<?php if (($count % 3) == 0): ?> card--preview-md<?php elseif (($count % 4) == 0): ?> card--preview-lg<?php endif; ?>">
+              <a href="<?php the_permalink(); ?>" rel="bookmark" class="card__link">
+                <div class="card__container">
 
+                  <?php if (has_post_thumbnail()): ?>
+                    <div class="card__image">
+                      <?php if (($count % 4) == 0): ?>
+                        <?php the_post_thumbnail('thumbnail-large'); ?>
+                      <?php else: ?>
+                        <?php the_post_thumbnail('thumbnail'); ?>
+                      <?php endif; ?>
+                    </div>
+                  <?php endif; ?>
+
+                  <div class="card__content card__content--truncate">
+                    <h3><?php the_title(); ?></h3>
+                    <?php the_excerpt(); ?>
+                  </div>
+
+                </div>
+              </a>
+            </article>
+
+          </div>
+        <?php
+          $count++;
+          endwhile;
+          wp_reset_postdata();
+        ?>
+      </div>
+    </div>
   </section>
         
 <?php else: ?>
   <?php get_template_part('template-parts/content', 'none'); ?>
 <?php endif; ?>
-
 
 <?php get_footer(); ?>
