a47d13c89c732873e7c15535da29871c7e12eed3
h0r0man <roman.horokhovatskyy@gmail.com>
2017-02-23 16:25:24 +0100

    Added layout for pages

--- template-sections/content-page.php
+++ template-sections/content-page.php
@@ -6,7 +6,13 @@
   ?>
 
   <div class="page__content">
-    <?php the_content(); ?>
+    <div class="container">
+      <div class="layout__row layout__row__xs--center layout__row__sm--center layout__row__md--center layout__row__lg--center">
+        <div class="layout__xs__12 layout__sm__8 layout__md__8 layout__lg__8">
+          <?php the_content(); ?>
+        </div>
+      </div>
+    </div>
   </div>
 
 </article>
