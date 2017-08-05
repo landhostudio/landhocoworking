263c1c2165c51fdda8b5f946589a6e384f5a9ef1
h0r0man <roman.horokhovatskyy@gmail.com>
2017-08-05 17:44:40 +0200

    Simplified walker menu

--- inc/menu.php
+++ inc/menu.php
@@ -2,84 +2,38 @@
 
 class walkerHeaderMenu extends Walker_Nav_Menu {
 
-	public function start_el(&$output, $item, $depth, $args) {
-		$output     .= '<li class="header__navigation__item'.($item->current ? ' header__navigation__item--active':'').'">';
-		$attributes  = '';
+  function start_el( &$output, $item, $depth = 0, $args = array(), $id = 0 ) {
+    $li_classes = array( 'header__navigation__item' );
 
-		! empty ( $item->attr_title )
-			// Avoid redundant titles
-			and $item->attr_title !== $item->title
-			and $attributes .= ' title="' . esc_attr( $item->attr_title ) .'"';
+    if ( $item->current ) {
+      $li_classes[] = 'header__navigation__item--active';
+    }
 
-		! empty ( $item->url )
-			and $attributes .= ' href="' . esc_attr( $item->url ) .'"';
+    $li_classes = implode( $li_classes, ' ' );
 
-		$attributes  = trim( $attributes );
-		$title       = apply_filters( 'the_title', $item->title, $item->ID );
-		$item_output = "$args->before<a $attributes>$args->link_before$title</a>"
-						. "$args->link_after$args->after";
+    $output .= "<li class='{$li_classes}'>";
+    $output .= "<a href='{$item->url}'>";
+    $output .= $item->title;
+    $output .= "</a>";
+  }
 
-		// Since $output is called by reference we don't need to return anything.
-		$output .= apply_filters(
-			'walker_nav_menu_start_el'
-			, $item_output
-			, $item
-			, $depth
-			, $args
-		);
-	}
-
-	public function start_lvl(&$output) {
-		$output .= '<ul class="sub-menu">';
-	}
-
-	public function end_lvl(&$output) {
-		$output .= '</ul>';
-	}
-
-	function end_el( &$output ) {
-		$output .= '</li>';
-	}
 }
 
 class walkerFooterMenu extends Walker_Nav_Menu {
 
-	public function start_el(&$output, $item, $depth, $args) {
-		$output     .= '<li class="footer__navigation__item layout__sm__6 layout__md__6 layout__lg__6'.($item->current ? ' footer__navigation__item--active':'').'">';
-		$attributes  = '';
+  function start_el( &$output, $item, $depth = 0, $args = array(), $id = 0 ) {
+    $li_classes = array( 'footer__navigation__item layout__sm__6 layout__md__6 layout__lg__6' );
 
-		! empty ( $item->attr_title )
-			// Avoid redundant titles
-			and $item->attr_title !== $item->title
-			and $attributes .= ' title="' . esc_attr( $item->attr_title ) .'"';
+    if ( $item->current ) {
+      $li_classes[] = 'footer__navigation__item--active';
+    }
 
-		! empty ( $item->url )
-			and $attributes .= ' href="' . esc_attr( $item->url ) .'"';
+    $li_classes = implode( $li_classes, ' ' );
 
-		$attributes  = trim( $attributes );
-		$title       = apply_filters( 'the_title', $item->title, $item->ID );
-		$item_output = "$args->before<a $attributes>$args->link_before$title</a>"
-						. "$args->link_after$args->after";
+    $output .= "<li class='{$li_classes}'>";
+    $output .= "<a href='{$item->url}'>";
+    $output .= $item->title;
+    $output .= "</a>";
+  }
 
-		// Since $output is called by reference we don't need to return anything.
-		$output .= apply_filters(
-			'walker_nav_menu_start_el'
-			, $item_output
-			, $item
-			, $depth
-			, $args
-		);
-	}
-
-	public function start_lvl(&$output) {
-		$output .= '<ul class="sub-menu">';
-	}
-
-	public function end_lvl(&$output) {
-		$output .= '</ul>';
-	}
-
-	function end_el( &$output ) {
-		$output .= '</li>';
-	}
 }
